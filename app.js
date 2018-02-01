395ffcd3ebdd19d00fca91981bc6a30d4536904f
Tamara <4744385@rts-365.dk>
2018-02-01 13:25:35 +0100

    com3

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 
+const logger = require('morgan'); //disse to linjer, er det der skal til, så så vi kan holde øje med om vi har skrevet korrekt eller om andet er gået galt undervejs.
+app.use(logger('drev')); // hvornår er det foregåret en ruquest og hvilken type request var det. 
+
 app.get('/api/tamara', (req, res) => {
     res.json({ navn: "Tamara", efternavn: "Fanayei", Bopæl: "Bryggen" });
 });
+
+
+app.get('/api/test', (req, res) => {
+    res.json({ text: "Hello World" });
+});
+
+app.get('/api/fanayei', (req, res) => {
+    res.json({ noget: "Hej" });
+});
+
+app.get('/api/espander', (req, res) => {
+    res.json({ andet: "farvel" });
+});
+
 
 app.use(express.static('public'));
 
@@ -11,7 +28,7 @@ app.listen(3000, (err) => {
     if (err) {
         console.log(err);
     }
-    console.log('App is listening on');
+    console.log('App is listening on http://localhost:3000');
 });
 
 
