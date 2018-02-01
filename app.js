2d949b46d32c0e55d907988a7cc03d9e12ab0bf9
Tamara <4744385@rts-365.dk>
2018-02-01 09:29:17 +0100

    com2

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/tamara', (req, res) => {
+    res.json({ navn: "Tamara", efternavn: "Fanayei", Bopæl: "Bryggen" });
+});
+
+app.use(express.static('public'));
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('App is listening on http://localhost:3000');
+});
+
+
+
